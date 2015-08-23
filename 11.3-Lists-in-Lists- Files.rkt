dd27e14b2950cb13bf932f2c178a264f2b083463
shenxs <247820400@qq.com>
2015-08-23 22:11:20 +0800

    加了首诗

---
+++ 11.3-Lists-in-Lists- Files.rkt
@@ -0,0 +1,4 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |11.3-Lists-in-Lists- Files|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+(read-file "ttt.txt")
