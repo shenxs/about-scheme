a0ef07d13fada9f515f4d46fbb62f02ef363691c
shenxs <247820400@qq.com>
2015-08-29 23:31:45 +0800

    想做二分查找但是不知道怎么写

---
+++ 12.3-Recursive-Auxiliary-Functions.rkt
@@ -0,0 +1,84 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 12.3-Recursive-Auxiliary-Functions) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+;n ,list of number -> list of number
+;将n插入到alon中,要求n>=它右边的数字
+(define (insert n alon)
+  (cond
+    [(empty? alon) (cons n alon)]
+    [else (if (>= n (first alon))
+            (cons n alon )
+            (cons (first alon) (insert n (rest alon) )))]))
+
+
+;list of number ->list of number
+;对一列数字进行排序
+(define (sort> alon)
+  (cond
+    [(empty? alon) '()]
+    [else (insert (first alon) (sort> (rest alon)))]))
+;(sort> (list 9 9 9 9 9 9 9 9 ))
+
+;;exercise 187
+;a program that sort list of email message by date
+;............................................ name
+
+(define-struct email [from date message])
+;;(make-email stirng number string)
+
+
+;;insert email by date
+(define (insert-email e aloe)
+  (cond
+    [(empty? aloe)(cons e '())]
+    [else (if (>= (email-date e) (email-date (first aloe)))
+            (cons e aloe)
+            (cons (first aloe) (insert-email e (rest aloe))))]))
+
+
+;;aloe->a sorted list of email by date
+
+(define (sort-email aloe)
+  (cond
+    [(empty? aloe) '()]
+    [else (insert-email (first aloe) (sort-email (rest aloe)))]))
+
+;;insert email by name
+(define (insert-name e aloe)
+  (cond
+    [(empty? aloe) '()]
+    [else (if (string<? (email-from e) (email-from (first aloe)))
+            (cons e aloe)
+            (cons (first aloe) (insert-name e (rest aloe))))]))
+
+(define (sort-name aloe)
+  (cond
+    [(empty? aloe) '()]
+    [else (insert-name (first aloe) (sort-name (rest aloe)) )]))
+
+
+;;exercise 188
+;sort game player by scores
+
+(define-struct game-player [name score])
+;;(make-game-player string number)
+
+;;game player, a list of player->a list of player
+(define (insert-player p alop)
+  (cond
+    [(empty? alop) (cons p alop)]
+    [else (if (>= (game-player-score p) (game-player-score (first alop)))
+            (cons p alop)
+            (cons (first alop) (insert-player p (rest alop))))]))
+
+(define (sort>player alop)
+  (cond
+    [(empty? alop) '()]
+    [else (insert-player (first alop) (sort>player (rest alop)))]))
+
+
+
+;;exercise 189
+;search-sorted ,quickly find a number in a sorted list of number
+;
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
  - racket 的学习练习记录，多为书上的exercise的记录。
  - 由于racket中文资料比较少，有需要的可以参考一下，共同学习
  - github上是代码，在我自己的[博客](http://blog.icetea.me)上有一些具体的学习记录
+## MIT
