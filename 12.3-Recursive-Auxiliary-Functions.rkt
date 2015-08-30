e534ace13b03bd2d63ea18ee082b244e733ac31c
shenxs <247820400@qq.com>
2015-08-30 17:21:57 +0800

    12.3 is over

--- 12.3-Recursive-Auxiliary-Functions.rkt
+++ 12.3-Recursive-Auxiliary-Functions.rkt
@@ -81,4 +81,32 @@
 
 ;;exercise 189
 ;search-sorted ,quickly find a number in a sorted list of number
+
+;number,alon->T/F
+;判断一个数字是否存在在一个从大到小的链表里
+(define (search-sorted n alon)
+  (cond
+    [(empty? alon) false]
+    [(= n (first alon)) true]
+    [(> n (first alon)) false]
+    [(< n (first alon)) (search-sorted n (rest alon))]))
+;(search-sorted 3 (list 9 9  2 1))
+
+;exercise 190
+;得到一个列表所有的前缀子列表
 ;
+(define (prefix l)
+  (cond
+    [(empty? l) '()]
+    [else (cons l (prefix (reverse (rest (reverse l)))))]))
+
+
+;;suffix
+;后缀子列表
+(define (suffix l)
+  (cond
+    [(empty? l) '()]
+    [else (cons l (suffix (rest l)))]))
+;(prefix (list 1 2 3 4 5 6))
+;(suffix (list 1 2 3 4 5 6))
+2015-08-30 17:20:34
