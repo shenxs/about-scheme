e312701fd5d33869196f8d3dbb76ccb67e1970f3
shenxs <247820400@qq.com>
2015-09-10 22:09:39 +0800

    又完成一个

--- 17.2.rkt
+++ 17.2.rkt
@@ -7,4 +7,34 @@
 ;list of Number-> list of Number
 ;list of X -> list of Y
 ;list of IR->list of String
-;
+ ;Exercise 241
+ ;[Number->Boolean]
+ (define (f1 n)
+   (odd? n))
+;[Boolean String ->String]
+(define (f2 b s)
+  (if b s "I have eat it hahahha ^-^?  "))
+;[Number,Number,Number->Number]
+(define (f3 n1 n2 n3)
+  (+ n1 n2 n3))
+;[Number ->list of Number]
+(define (f4 n)
+  (cond
+    [(zero? n) '()]
+    [else (cons n (f4 (sub1 n)))]))
+;[list of Number ->Boolean]
+(define (f5 l )
+  (cond
+    [(empty? (rest l)) true]
+    [else (and (< (first l) (first (rest l))) (f5 (rest l)))]))
+;Exercise 242
+;[list of Number ,[Number,Number->boolean] ->list of Number]
+;[list of string ,[String,String-> Boolean]->list of String]
+;[lsit of X ,[X ,X -> Boolean] -> list of X]
+;[lsit of IR,[IR,IR->boolean]->llst of IR]
+
+
+;Exercise 243
+;[list of Number [Number-> Number]->list of Number]
+;[list of sting [string ->string]->list of string]
+;[list of X [X->X] ->list of X]
