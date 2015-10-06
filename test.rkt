f8b54ed16514acd99277919b3b49323206733a30
shenxs <247820400@qq.com>
2015-10-06 11:48:56 +0800

    测试用文件

--- test.rkt
+++ test.rkt
@@ -1,6 +1,1 @@
 #lang racket
- (define (foo a l)
-           (cond
-             [(= a (first l)) (cons a '())]
-             [else (cons (first l) (foo a  (rest l)))]))
-(foo '() (list 1 2 3 4 5 6))
