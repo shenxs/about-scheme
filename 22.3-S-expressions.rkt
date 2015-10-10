1efcb5e81beb80bf717637dfd64fb82a3b866fe1
shenxs <247820400@qq.com>
2015-10-10 19:08:00 +0800

    进度有点慢

--- 22.3-S-expressions.rkt
+++ 22.3-S-expressions.rkt
@@ -12,6 +12,8 @@
 ; – Symbol
 
 ;Exercise 302
+
+
 (define (atom? X)
   (cond
     [(symbol? X) true]
@@ -23,20 +25,52 @@
 ;in some S-expression sexp
 ;S-expression Symbol ->Number
 ;计算S表达式中有多少sy Symbol
+
+
+;;三个函数分离
 (define (count sexp sy)
   (cond
     [(atom? sexp) (count_atom sexp sy)]
     [else (count_sl sexp sy)]))
-
 (define (count_atom at sy)
   (cond
     [(symbol? at) (if (symbol=? at sy) 1 0 )]
     [(string? at) 0]
     [(number? at) 0]))
-
 (define (count_sl sl sy )
   (cond
     [(empty? sl) 0 ]
     [else (+ (count (first sl) sy) (count_sl (rest sl) sy))]))
 
-(count '(wing (wing (wing body wing) wing) wing) 'wing )
+
+;Exercise 303
+
+(define (count_v2 sexp sy)
+  (local (
+          ; s表达式 smybol ->N
+          ; 主函数
+          (define (count-sexp sexp sy)
+            (cond
+              [(atom? sexp) (count-atom sexp sy)]
+              [else (count_sl sexp sy)]))
+
+          ; SL Symbol ->N
+          ; 计算SL中sy的数量
+          (define (count-sl sl sy)
+            (cond
+              [(empty? sl) 0]
+              [else (+ (count-sexp (first sl) sy)
+                       (count-sl (rest sl) sy))]))
+
+          ;Atom Symbol -> N
+          ;计算at中的sy的数量
+          (define (count-atom at sy)
+            (cond
+              [(number? at) 0]
+              [(string? at) 0]
+              [(symbol? at) (if (symbol=? at sy) 1 0)]))
+          )
+    (count-sexp sexp sy)))
+
+
+
