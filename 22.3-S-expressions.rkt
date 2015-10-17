4b8312f1d969da8675dfa7ce02eca8049ded8ec9
shenxs <247820400@qq.com>
2015-10-17 17:30:22 +0800

    计算s表达式深度

--- 22.3-S-expressions.rkt
+++ 22.3-S-expressions.rkt
@@ -1,4 +1,6 @@
-#lang racket
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname 22.3-S-expressions) (read-case-sensitive #t) (teachpacks ((lib "abstraction.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "abstraction.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
 
 ; An S-expr (S-expression) is one of:
 ; – Atom
@@ -44,7 +46,10 @@
 
 
 ;Exercise 303
-
+;sl is one of
+;(atom atom ....atom)
+;(atom sl)
+;
 (define (count_v2 sexp sy)
   (local (
           ; s表达式 smybol ->N
@@ -72,5 +77,22 @@
           )
     (count-sexp sexp sy)))
 
+;计算s表达式的深度
+;atom元素的深度等于1
 
+;sl->Number
 
+;a ->1
+;(z b) ->1
+;(1 2 4 (2 (2 4 ) 5) 1  ) ->3
+;(1 2 ( 3 3 ) (3 (5 6)))  ->3
+
+(define (depth sl)
+  (cond
+    [(atom? sl) 1]
+    [(empty? sl) 0 ]
+    [else (max (+ 1 (depth (first sl))) (depth (rest sl)))]))
+         
+(depth '(1 2 4 (2 (2 ( + 6 (6 9)) ) 5) 1  ))
+
+(depth '(a))
