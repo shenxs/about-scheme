7140feb342b10549c8bf67a3d4570d05ffd52ce8
shenxs <247820400@qq.com>
2015-11-01 02:15:34 +0800

    牛顿解方程

--- 23.2-Refining-Data-Definitions.rkt
+++ 23.2-Refining-Data-Definitions.rkt
@@ -6,3 +6,42 @@
 ;这个文件夹在哪里?
 ;文件夹里面有有什么?
 ;文件夹的所有者是谁?
+
+
+;Model1
+;; A Dir.v1 (short for directory) is one of:
+; – '()
+; – (cons File.v1 Dir.v1)
+; – (cons Dir.v1 Dir.v1)
+; A File.v1 is a Symbol.
+
+;Exercise 316
+;(cons 'file (cons 'file2 '() )))))
+
+
+;Exercise 317
+
+(define (how-many dir)
+  (cond
+    [(empty? dir) 0]
+    [(symbol? dir) 1]
+    [else (+ (how-many (first dir)) (how-many (rest dir)))]))
+
+(how-many (cons 'flie1 (cons 'file2 (cons 'file3 '()))))
+
+
+;现在我们就要细化我们的模型啦
+;Molde2
+
+(define-struct dir [name content])
+
+;所以我们的模型就便成
+;; A Dir.v2 is a structure:
+;   (make-dir Symbol LOFD)
+
+; A LOFD (short for list of files and directories) is one of:
+; – '()
+; – (cons File.v2 LOFD)
+; – (cons Dir.v2 LOFD)
+
+; A File.v2 is a Symbol.
