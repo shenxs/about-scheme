a0340b5d46c5fb9d00f420c142eed584ed0cfbc2
richard <a247820400@gmail.com>
2016-01-24 21:39:01 +0800

    完成准备工作(XML的格式数据的定义处理完毕),开始处理XML

--- 25-XML.rkt
+++ 25-XML.rkt
@@ -11,9 +11,9 @@
 
 
 #| '(machine ((initial "red")) |#
-          #| (action ((state "red") (next "green"))) |#
-          #| (action ((state "green") (next "yellow"))) |#
-          #| (action ((state "yellow") (next "red")))) |#
+#| (action ((state "red") (next "green"))) |#
+#| (action ((state "green") (next "yellow"))) |#
+#| (action ((state "yellow") (next "red")))) |#
 
 #| ;用symbol来表示Xexpr(即X-expression) |#
 
@@ -31,9 +31,37 @@
 #| '(Symbol string) |#
 
 #| ;Exercise 351 |#
-#| '(cons transition (cons (cons '(from "seen-e") '(to "seen-f")) '())) |#
-#| '(cons ul (cons li (cons word (cons word '())))) |#
+;<transition from="seen-e" to="seen-f" />
+;'(transition '((from "seen-e") (to "seen-f")))
+;<ul>
+;<li><word /><word /></li>
+;<li><word /></li>
+;</ul>
 
+
+;'(ul (li word) (li word))
+;'(end)
+;;
+;an Xexper.v2 is
+;- (cons Symbol (cons Xexpr.v2 (cons Xexpr.v2 (cons Xexper.v2 ...))))
+;- (cons Symbol (cons (cons Attribute (cons Attribute (cons Attribute)))  (cons Xexper.v2 (cons Xexper.v2 (cons Xexper.v2 (cons .....))))))
+;- '(Symbol '(a a a ) x x x x)
+;- '(Symbol x x x x x)
+;an Attribute is
+;-- (cons Symbol (cons Srting '()))
+;-- '(Symbol String)
+;Exercise 353. Interpret the following elements of Xexpr.v2 as XML data:
+;'(server ((name "example.org")))
+;<server name="example.org" ></server>
+;'(carcassonne (board (grass)) (player ((name "sam"))))
+;<carcassonne>
+;   <board>
+;       <grass></grass>
+;   </board>
+;   <player name="sam"></player>
+;</carcassonne>
+;'(start)
+;<start></start>
 (define a0 '((initial "red")))
 
 (define e0 '(machine))
@@ -60,6 +88,7 @@
             may_attributes
             '()))])))
 
+(xexpr-attributes e4)
 
 (define (xexpr-name xe)
   (first xe))
@@ -73,6 +102,23 @@
                 (rest optional-loa+content)
                 optional-loa+content))])))
 
+;Exercise 355
+;; (define (list_of_attributes? x)
+  ;; (or (empty? x) (cons? (first x))))
+;因为这不是一个递归的定义
+;Exercise 356
+;
+;Exercise 357
+;list of attributes and a Symbol x -> String/false
+;给定一个list,存放许多的参数,决定Symbol X 对应的参数的值是多少,如果没有就返回false,如果有返回值应该会是一个String值
+(define (lookup-attributes l x)
+  (local ((define pair-or-false (assq x l)))
+    (if (false? pair-or-false)
+      pair-or-false
+      (rest pair-or-false))))
+
+(lookup-attributes '((ha "123") (jk "123")) 'qw)
+
 ;Exercise 358
 
 ;word?
@@ -81,12 +127,12 @@
 ;xword 的格式为'(word ((text string)))
 ;(word ((text "one")))
 ;(word ((text "two"))))
-(define (xword? x)
+(define (word? x)
   (and (symbol=? 'word (first x))
        (symbol=? 'text (first (first (second x))))))
 
 (define (word-text x)
-  (if (xword? x)
+  (if (word? x)
     (second (first (second x)))
     (error "这不是一个xword")))
 
