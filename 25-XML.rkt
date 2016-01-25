e72caf958a86859efebda8b26ba7a8dbc82a38e1
richard <a247820400@gmail.com>
2016-01-25 16:31:16 +0800

    似乎还有些不怎么理解,再看看估计差不多

--- 25-XML.rkt
+++ 25-XML.rkt
@@ -1,4 +1,7 @@
-#lang racket
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname 25-XML) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t quasiquote repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+
 ;这章是讲处理xml文档的
 ;xml可能已经过时了,但是json等等的处理原理相通
 
@@ -70,13 +73,20 @@
 (define e3 '(machine () (action)))
 (define e4 `(machine ,a0 (action) (action)))
 
+(define ex
+  '(ul
+    (li (word ((text "one"))))
+    (li (word ((text "two"))))))
 
 
-
+;Exercise 355
+;; (define (list_of_attributes? x)
+  ;; (or (empty? x) (cons? (first x))))
 (define (list_of_attributes? x)
   (cond
     [(empty? x) true]
     [else (cons? (first x))]))
+
 
 (define (xexpr-attributes xe)
   (local ((define optional-loa+content (rest xe)))
@@ -102,9 +112,7 @@
                 (rest optional-loa+content)
                 optional-loa+content))])))
 
-;Exercise 355
-;; (define (list_of_attributes? x)
-  ;; (or (empty? x) (cons? (first x))))
+
 ;因为这不是一个递归的定义
 ;Exercise 356
 ;
@@ -129,13 +137,121 @@
 ;(word ((text "two"))))
 (define (word? x)
   (and (symbol=? 'word (first x))
-       (symbol=? 'text (first (first (second x))))))
+       (symbol=? 'text (first (first (second x))))
+       (string? (second (first (second x))))))
 
 (define (word-text x)
   (if (word? x)
     (second (first (second x)))
     (error "这不是一个xword")))
 
+(define SIZE 12)
+(define COLOR 'black)
+(define BULLET
+    (beside (circle 1 'solid 'black) (text " " SIZE COLOR)))
+;; (define little-dot(circle 2 'solid "black"))
+;; (define BULLET
+  ;; (beside/align 'center little-dot (rectangle 12 12 'outline "white")))
+(define item1-rendered
+  (beside/align 'center BULLET  (text "one" 15 'black)))
+(define item2-rendered
+  (beside/align 'center BULLET (text "two" 15 'black)))
+(define e0-rendered
+  (above/align 'left item1-rendered item2-rendered))
+;在vim中调试看不到图形界面,然而vim使用起来又是那么的顺手的
+;希望有ide可以保留vim的配置文件,但是又可以将ide自身的特点保留
+
+;25.2 Rendering XML Enumerations
+;这章我们要将xml转换为图片
+; An XEnum.v1 is one of:
+; – (cons 'ul [List-of XItem.v1])
+; – (cons 'ul (cons [List-of Attribute] [List-of XItem.v1]))
+; An XItem.v1 is one of:
+; – (cons 'li (cons XWord '()))
+; – (cons 'li (cons [List-of Attribute] (cons XWord '())))
+
+
+; XItem.v1 -> Image
+; renders a single item as a "word" prefixed by a bullet
+(define (render-item1 i)
+  (local ((define content (xexpr-content i))
+          (define element (first content))
+          (define word-in-i (word-text element)))
+    (beside/align 'center BULLET (text word-in-i 12 'black))))
+
+
+; XEnum.v1 -> Image
+; renders a simple enumeration as an image
+
+
+(define (render-enum1 xe)
+  (local (
+          (define content (xexpr-content xe) )
+          ;Xitem.v1->image
+          (define (deal_with_one_item fst_itm so_far)
+            (above/align 'left so_far (render-item1 fst_itm)))
+          )
+    (foldr deal_with_one_item empty-image content)))
+(render-enum1 ex)
+
+;实际上我们会遇到在li元素里面出现ul元素的情况,这是一个嵌套的定义
+; An XItem.v2 is one of:
+; – (cons 'li (cons XWord '()))
+; – (cons 'li (cons [List-of Attribute] (cons XWord '())))
+; – (cons 'li (cons XEnum.v2 '()))
+; – (cons 'li (cons [List-of Attribute] (cons XEnum.v2 '())))
+; An XEnum.v2 is one of:
+; – (cons 'ul [List-of XItem.v2])
+; – (cons 'ul (cons [List-of Attribute] [List-of XItem.v2]))
 
 
 
+(define (render-enum xe)
+  (local ((define content (xexpr-content xe))
+          ; XItem.v2 Image -> Image
+          (define (deal-with-one-item fst-itm so-far)
+            (above/align 'left (render-item fst-itm) so-far)))
+    (foldr deal-with-one-item empty-image content)))
+
+;由于嵌套定义对于enum并没有影响,所以需要改变的是对于item元素的处理
+;'(li
+;       (word (text "one"))
+;       (ul
+;           (li (word (text "two")))))
+(define (render-item an-item)
+  (local ((define content (first (xexpr-content an-item))))
+     (cond
+       [(word? content)
+        (beside/align 'center
+                              BULLET
+                              (text (word-text content) SIZE 'black))]
+       [else (render-enum content)])))
+
+(define ceshi
+  '(ul
+    (li
+       (word (text "one")))
+       (ul
+           (li (word (text "two"))))))
+(render-enum ceshi)
+;Exercise 361
+;直接从v2版本的定义开始构建rander函数估计是一样的
+;所以略过
+;Exercise 362
+;不太喜欢原版还是不在cond外面加上beside/align看着逻辑更加清晰
+;加上似乎代码更加漂亮
+
+
+;Exercise 363
+;数一下enum中一共有几个hello
+;enum->Number
+
+;; (define (count-hello xe)
+  ;; (local ((define content (xexpr-content xe))
+          ;; (define (add-hello fst-item so-far)
+            ;; (+ (count-item fst-item) so-far)))
+    ;; (foldr add-hello 0 content)))
+
+;; (define (count-item an_item)
+  ;; (local ((define (content (first (xexpr-content an_item)))))
+    ;; ))
