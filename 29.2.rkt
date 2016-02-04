7f60d938b81cd35fbadfd761a48a238ac0d60520
richard <a247820400@gmail.com>
2016-02-04 16:57:11 +0800

    快排,ok

--- 29.2.rkt
+++ 29.2.rkt
@@ -1,21 +1,21 @@
 #lang racket
-
-
-(define (quick-sort alon)
+(define (quick-sort alon f)
   (local (
           (define (judge f l n)
-            (filter (λ (x) (if (f x n) #t #f))
-                    l)))
+            (filter (λ (x) (if (f x n) #t #f))  l)))
     ;;;----IN----
     (cond
       [(empty? alon) '()]
+      [(= 1 (length alon)) alon]
+      [(< (length alon) 10) (sort alon <)]
       [else (append
-              (quick-sort (judge < (rest alon) (first alon)))
+              (quick-sort (judge f (rest alon) (first alon)) f )
               (list (first alon))
-              (quick-sort (judge >= (rest alon) (first alon))))])))
+              (quick-sort (judge (λ (a b) (not (f a b)) ) (rest alon) (first alon)) f))])))
+
 (define (random-list n M)
   (cond
     [(= n 0) '()]
     [else (cons (random M) (random-list (sub1 n) M))]))
 
-(define a (quick-sort (random-list 10000 10000)))
+(quick-sort (random-list 100 10000) >)
