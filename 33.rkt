fc3fa03177c7b0b61144b083e8cc6e0b0aa49603
richard <a247820400@gmail.com>
2016-02-14 14:45:08 +0800

    abstract find-my-path/list using ormap

--- 33.rkt
+++ 33.rkt
@@ -53,7 +53,7 @@
               [(boolean? candidate) (find-path/list (rest lo-Os) D G)]
               [else candidate]))]))
 
-(find-path 'A 'G graph)
+;; (find-path 'A 'G graph)
 
 
 ;; 起点,终点,图===>#false 或正确的联通路径
@@ -66,17 +66,19 @@
                  (define maybe (find-my-path/list next e g)))
             (cond
               [(cons? maybe) (cons s maybe)]
-              [else #false]))]))
+              [else #f]))]))
 
 ;[list of nodes] [end node] [graph] ===>#f or [list of nodes]
 (define (find-my-path/list lon e g)
   (cond
     [(empty? lon) #f]
     [else (local (
-                  (define maybe (find-my-path (first lon) e g)))
-            (cond
-              [(cons? maybe) maybe]
-              [else (find-my-path/list (rest lon) e g)]))]))
+                  (define maybe-list
+                    (map (λ (x) (find-my-path x e g)) lon))
+                  (define (d? item)
+                    (if (cons? item) item #f)))
+            (ormap d? maybe-list))]))
+
 (find-my-path 'A 'G graph)
 
 ;Exercise 446
@@ -109,4 +111,11 @@
          )
     (test-all lon graph)))
 
-(test-on-all-nodes graph)
+;; (test-on-all-nodes graph)
+
+;; 定义一个带有环的图的例子
+(define circle-graph
+  '((A (B C))
+    (B (C))
+    (C (A))))
+
