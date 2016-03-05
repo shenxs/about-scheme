a175a3e5144e1be5311ebf2982f13e22cf6b7858
shensx <a247820400@gmail.com>
2016-03-05 23:31:17 +0800

    传教士过河--over

--- 38.2.rkt
+++ 38.2.rkt
@@ -1,4 +1,6 @@
 #lang racket
+(require 2htdp/image
+         2htdp/universe)
 
 (define-struct pair [left right])
 (define our_cons? pair?)
@@ -21,9 +23,9 @@
 
 (define (my_cons x y)
   (λ (choose)
-     (cond
-       [(= choose 1) x]
-       [else y])))
+    (cond
+      [(= choose 1) x]
+      [else y])))
 
 (define (my_first a_list)
   (a_list 1))
@@ -71,25 +73,34 @@
 ;; '((3 3) l (0 0))
 ;最终状态
 ;; '((0 0) r (3 3))
+
+;;迭代类型
+;在数据结构中加入迭代器
+;; '((left-white left-black) 'where-isthe-boat (right-white right-black) (list-of-history))
 (define (final? state)
   (if (and
-        ((first (first state)) . = . 0)
-        ((second (first state)) . = . 0)
-        (symbol=? 'r (second state))
-        ((first (third state)) . = . 3)
-        ((second (third state)) . = . 3))
-    #t #f))
+       ((first (first state)) . = . 0)
+       ((second (first state)) . = . 0)
+       (symbol=? 'r (second state))
+       ((first (third state)) . = . 3)
+       ((second (third state)) . = . 3))
+      #t #f))
 
 ;;用于改变s state
 ;f1 f2 f3 f4 都是对于数字的处理
 ;基本就是add1 sub1 f(定义在下面)之类 通过组合不同dev处理方法得到新的游戏状态
+;并且将其加入历史状态
 (define (c-s s f1 f2 f3 f4)
   (list (list (f1 (first (first s)))
               (f2 (second (first s))))
         (if (symbol=? 'l (second s))
-          'r 'l)
+            'r 'l)
         (list (f3 (first (third s)))
-              (f4 (second (third s))))))
+              (f4 (second (third s))))
+        (append (list (list (first s) (second s) (third s)))
+                (if (empty? (fourth s))
+                    '()
+                    (fourth s) ))))
 (define f
   (lambda (x) x))
 
@@ -98,25 +109,87 @@
 (define (add2 n)
   (+ n 2))
 
+
+
+(define (member? item l)
+  (cond
+    [(empty? l) #f]
+    [(equal? item (first l)) #t]
+    [else (member? item (rest l))]))
+
+(define (possible? s)
+  (let ([l1 (first (first s))]
+        [l2 (second (first s))]
+        [r1 (first (third s))]
+        [r2 (second (third s))]
+        [history (fourth s)])
+    (and (>= l1 0) (>= l2 0) (>= r1 0) (>= r2 0)
+         (not (member? (list (list l1 l2) (second s) (list r1 r2)) history ))
+         (or (<= l1 l2) (= 0 (* l1 l2)))
+         (or (<= r1 r2) (= 0 (* r1 r2)))
+         )))
+
+(possible? '((3 3) 'l (0 0) '()))
+
 ;;[list of status]===>[list of status]
 ;在处理时要考虑l,r不同的情况,加入是否要被食人族吃掉的判断,也不可以出现负数,加入迭代器之后不能和以前的游戏状态重复`
 (define (create-next-states* los)
   (local ((define (creat-next-state s)
-            (list (c-s s sub1 sub1 add1 add1)
-                  (c-s s sub1 f add1 f)
-                  (c-s s f sub1 f add1)
-                  (c-s s sub2 f add2 f)
-                  (c-s s f sub2 f add2))))
-    (foldr append '() (map creat-next-state los))) )
+            (if (symbol=? 'l (second s))
+                (list (c-s s sub1 sub1 add1 add1)
+                      (c-s s sub1 f add1 f)
+                      (c-s s f sub1 f add1)
+                      (c-s s sub2 f add2 f)
+                      (c-s s f sub2 f add2))
+                (list (c-s s add1 add1 sub1 sub1)
+                      (c-s s add1 f sub1 f)
+                      (c-s s f add1 f sub1)
+                      (c-s s add2 f sub2 f)
+                      (c-s s f add2 f sub2)
+                      ))))
+     (filter possible? (foldr append '() (map creat-next-state los)))))
+
+(create-next-states* '(((2 2) r (1 1) (((3 3) l (0 0)) quote ()))))
 
 (define (solve state0)
   (local (; [List-of PuzzleState] -> PuzzleState
           ; generative generate the successor states for all intermediate ones
           (define (solve* los)
             (cond
+              [(empty? los) (error "出错")]
               [(ormap final? los) (first (filter final? los))]
-              [else (solve* (create-next-states* los))])))
+              
+              [else 
+                     (solve* (create-next-states* los))])))
     (solve* (list state0))))
 
-(solve '((3 3) l (0 0)))
+(define answer (solve '((3 3) l (0 0) '())))
 
+(define w (circle 5 'solid 'gray))
+(define b (circle 5 'solid 'balck))
+
+(define (over-put item n)
+  (cond
+    [(= n 0) (empty-scene 10 30)]
+    [(= n 1) item]
+    [else (above item (over-put item (sub1 n)))]))
+
+(define (render-mc s)
+  (beside (over-put b (first (first s)))
+          (over-put w (second (first s)))
+          (empty-scene 60 30)
+          (over-put b (first (third s)))
+          (over-put w (second (third s)))
+          ))
+
+(define list-of-s
+  (append
+   (list
+    (list (first answer) (second answer) (third answer)))
+   (fourth answer )       ))
+(define ahh (rest  (rest (reverse list-of-s))))
+
+
+(define pictures (map render-mc ahh))
+
+(run-movie 7 pictures)
