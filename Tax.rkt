d641fcaea2e0de70336ac6fc05ab68b394cc1412
shenxs <a247820400@gmail.com>
2016-09-07 15:03:57 +0800

    整理文件结构

--- Tax.rkt
+++
@@ -1,9 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname Tax) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t write repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")))))
-(define (Tax x)
-  (cond
-    [(< 0 x 1000) 0]
-    [(<= 1000 x 10000)(* (- x 1000) 0.05)]
-    [else (+ (* 0.05 9000) (* 0.08 (- x 10000)))]))
-
