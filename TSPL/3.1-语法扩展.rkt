a018575108b209477003d82303372c0b34bbd5c7
shenxs <a247820400@gmail.com>
2016-09-16 00:45:55 +0800

    and or的语法定义

--- TSPL/3.1-语法扩展.rkt
+++ TSPL/3.1-语法扩展.rkt
@@ -7,7 +7,38 @@
 
 ;核心语法包括 define 常量 变量 过程 quote表达式 λ表达式 if表达式 set!表达式
 
+
+;;TODO f5运行 和在repl解释的结果不太一样
 (begin 'a 'b 'c)
 ;可以转化为
 ((lambda () 'a 'b 'c))
+
+
+;; ...允许0个或者是更多的表达式
+
+(define-syntax and
+  (syntax-rules ()
+    [(_) #t]
+    [(_ e) e]
+    [(_ e1 e2 e3 ...)
+     (if e1
+         (and e2 e3 ...)
+         #f)]))
+
+;; 或表达式
+
+(define-syntax or
+  (syntax-rules ()
+    [(_) #f]
+    [(_ e) e]
+    [(_ e1 e2 e3 ...)
+     (if e1 e1 (or e2 e3 ...))]))
+
+
+;;3.1.1
+
+
+((lambda (x) (and x (memv 'b x))) (memv 'a ls))
+
+((lambda (x) (if x (memv 'b x) #f)) (memv 'a ls))
 
