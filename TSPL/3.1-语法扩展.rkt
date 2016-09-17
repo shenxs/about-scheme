acd5e1c59aff94a8fefd96dd1122ca03eff170bd
shenxs <a247820400@gmail.com>
2016-09-17 15:06:51 +0800

    语法扩展完成

--- TSPL/3.1-语法扩展.rkt
+++ TSPL/3.1-语法扩展.rkt
@@ -34,6 +34,20 @@
     [(_ e1 e2 e3 ...)
      (if e1 e1 (or e2 e3 ...))]))
 
+(or (begin (display "hello")
+           #t)
+    #f)
+
+;;
+(define-syntax or
+  (syntax-rules ()
+    [(_) #f]
+    [(_ e) e]
+    [(_ e1 e2 e3 ...)
+     (let ([t e1])
+       (if t t (or e2 e3 ...)))]))
+
+;;多了一句let为什么要多一层 TODO
 
 ;;3.1.1
 
@@ -42,3 +56,59 @@
 
 ((lambda (x) (if x (memv 'b x) #f)) (memv 'a ls))
 
+
+;;3.1.2
+
+(or (memv x '(a b c)) (list x))
+
+;;====>
+
+(let ([t (memv x '(a  b c))])
+  (if t t (list x)))
+
+;;多一层是为了防止副作用吗?
+;;是的
+(let ([t (begin (display "hello")
+                #f)])
+  (if t t "world"))
+
+;;3,1,3
+
+
+(define-syntax let*
+  (syntax-rules ()
+    [(_ ([v e])
+        t)
+     (let ([v e])
+       t)]
+    [(_ ([v0 e0] [v1 e1] [v2 e2] ...)
+        t)
+     (let ([v0 e0])
+       (let* ([v1 e1] [v2 e2] ...)
+         t))]))
+
+(let* ([a 5]
+       [b (+ a a)]
+       [c (+ a b)])
+  (list a b c))
+
+
+
+;;3.1.4
+
+
+
+(define-syntax when
+  (syntax-rules ()
+    [(_ test exp1 exp2 ...)
+     (if test (begin exp1 exp2 ...) (void))]))
+
+(define-syntax unless
+  (syntax-rules ()
+    [(_ test exp1 exp2 ...)
+     (when (not test) exp1 exp2 ...)]))
+
+(let ([x 3])
+  (unless (= x 0) (set! x (+ x 1)))
+  (when (= x 4) (set! x (* x 2)))
+  x)
--- TSPL/3.1-语法扩展.ss
+++ TSPL/3.1-语法扩展.ss
@@ -1,1 +1,20 @@
 (begin 'a 'b 'c)
+
+
+;;3.3.1
+(define-syntax let*
+  (syntax-rules ()
+    [(_ ([v e])
+       t)
+     (let ([v e])
+       t)]
+    [(_ ([v0 e0] [v1 e1] [v2 e2] ...)
+        t)
+     (let ([v0 e0])
+       (let* ([v1 e1] [v2 e2] ...)
+         t))]))
+
+(let* ([a 5]
+       [b (+ a a)]
+       [c (+ a b)])
+  (list a b c))
