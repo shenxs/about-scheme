ed4c9cf30072a1595efbd60a08d1c797fde5e604
Richard <a247820400@gmail.com>
2018-07-25 21:06:08 +0800

    添加平台自定义函数

--- (build my lisp)/main.c
+++ (build my lisp)/main.c
@@ -1,9 +1,26 @@
 #include<stdio.h>
 #include <stdlib.h>
 
+#ifdef _WIN32
+#include <string.h>
+static char buffer[2048]
+
+/* 自定义readline函数 */
+char* readline(char* prompt){
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+/*  自定义add_history function */
+void add_history(char* unused) {}
+
+#else
 #include <editline/readline.h>
-
-
+#endif
 
 int main(int argc,char** argv){
   puts("MyLisp Version 0.0.0.0.1");
