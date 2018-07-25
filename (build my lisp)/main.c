63c70336cad8a38d9239763688f6b540ec097fec
Richard <a247820400@gmail.com>
2018-07-25 22:48:12 +0800

    逆波兰表达式

--- (build my lisp)/main.c
+++ (build my lisp)/main.c
@@ -23,6 +23,9 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #endif
 
+long eval(mpc_ast_t* t);
+long eval_op(long x,char* op ,long y);
+
 int main(int argc,char** argv){
 
 
@@ -51,7 +54,8 @@ int main(int argc,char** argv){
 
     mpc_result_t r;
     if(mpc_parse("<stdin>", input, Lispy, &r)){
-      mpc_ast_print(r.output);
+      long result= eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     }else{
       mpc_err_print(r.error);
@@ -61,5 +65,32 @@ int main(int argc,char** argv){
   }
 
   mpc_cleanup(4, Number,Operator,Expr,Lispy);
+  return 0;
+}
+long eval(mpc_ast_t* t){
+  /* 数字直接返回值 */
+  if(strstr(t->tag, "number")){
+    return  atoi(t->contents);
+  }
+
+  /* 表达式，计算其值 */
+  char* op=t->children[1]->contents;
+
+  long x =eval(t->children[2]);
+
+  int i=3;
+  while(strstr(t->children[i]->tag, "expr")){
+    x=eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+  return x;
+
+}
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0 || strcmp(op, "add") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0 || strcmp(op, "sub") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0 || strcmp(op, "mul") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0 || strcmp(op, "div") == 0) { return x / y; }
+  if (strcmp(op, "%") == 0) { return x % y; }
   return 0;
 }
