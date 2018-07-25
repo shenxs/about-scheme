94353752736ab81a9db8628153054420a9d46252
Richard <a247820400@gmail.com>
2018-07-25 21:39:35 +0800

    添加语法分析

--- (build my lisp)/main.c
+++ (build my lisp)/main.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include <stdlib.h>
+#include "mpc.h"
 
 #ifdef _WIN32
 #include <string.h>
@@ -23,6 +24,23 @@ void add_history(char* unused) {}
 #endif
 
 int main(int argc,char** argv){
+
+
+  /* 创建parsers */
+  mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr= mpc_new("expr");
+  mpc_parser_t* Lispy=mpc_new("lispy");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+            "                                                     \
+    number   : /-?[0-9]+/ ;                             \
+    operator : '+' | '-' | '*' | '/' ;                  \
+    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+    lispy    : /^/ <operator> <expr>+ /$/ ;             \
+  ",
+            Number,Operator,Expr,Lispy);
+
   puts("MyLisp Version 0.0.0.0.1");
   puts("Press Ctrl+c to exit");
 
@@ -31,8 +49,17 @@ int main(int argc,char** argv){
 
     add_history(input);
 
-    printf("%s\n", input);
+    mpc_result_t r;
+    if(mpc_parse("<stdin>", input, Lispy, &r)){
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    }else{
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
     free(input);
   }
+
+  mpc_cleanup(4, Number,Operator,Expr,Lispy);
   return 0;
 }
