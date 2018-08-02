6d3c38eed6412c7a16b9ece8b86099a0ad4f680a
Richard <a247820400@gmail.com>
2018-08-02 22:16:56 +0800

    完成s表达式的eval

--- (build my lisp)/main.c
+++ (build my lisp)/main.c
@@ -154,6 +154,89 @@ void lval_println(lval *v){
   lval_print(v);printf("\n");
 }
 
+
+lval* lval_eval_sexpr(lval* v);
+
+lval* lval_eval(lval *v){
+  if(v->type ==LVAL_SEXPR) {return lval_eval_sexpr(v);}
+  return v;
+}
+
+
+lval* lval_pop(lval* v,int i){
+  lval * x =v->cell[i];
+
+  memmove(&v->cell[i], &v->cell[i+1] ,sizeof(lval*)*(v->count-i-1));
+  v->count--;
+  v->cell = realloc(v->cell,sizeof(lval*)*v->count);
+  return x;
+}
+
+lval* lval_take(lval* v,int i){
+  lval* x = lval_pop(v,i);
+  lval_del(v);
+  return x;
+}
+
+
+lval* buildin_op(lval* a,char* op){
+  /* 确保所有的参数都是数字 */
+  for(int i=0;i<a->count;i++){
+    if(a->cell[i]->type!=LVAL_NUM){
+      lval_del(a);
+      return lval_err("can not deal with non-number");
+    }
+  }
+
+  lval* x =lval_pop(a,0);
+  if((strcmp(op,"-")==0)&& a->count ==0){
+    x->num = -x->num;
+  }
+
+  while(a->count>0){
+    lval* y = lval_pop(a,0);
+    if (strcmp(op, "+") == 0) { x->num += y->num; }
+    if (strcmp(op, "-") == 0) { x->num -= y->num; }
+    if (strcmp(op, "*") == 0) { x->num *= y->num; }
+    if (strcmp(op, "/") == 0) {
+      if (y->num == 0) {
+        lval_del(x); lval_del(y);
+        x = lval_err("Division By Zero!"); break;
+      }
+      x->num /= y->num;
+    }
+    lval_del(y);
+  }
+
+  lval_del(a);return x;
+}
+
+lval* lval_eval_sexpr(lval* v){
+  for(int i=0;i<v->count;i++){
+    v->cell[i]=lval_eval(v->cell[i]);
+  }
+
+  for(int i=0;i<v->count;i++){
+    if(v->cell[i]->type ==LVAL_ERR){
+      return lval_take(v,i);
+    }
+  }
+
+  if(v->count==0){return v;}
+  if(v->count==1){return lval_take(v,0);}
+
+  lval* f = lval_pop(v,0);
+  if(f->type != LVAL_SYM){
+    lval_del(f);lval_del(v);
+    return lval_err("S-expression does not start with symbol!");
+  }
+
+  lval* result = buildin_op(v,f->sym);
+
+  lval_del(f);
+  return result;
+}
+
 int main(int argc,char** argv){
 
   /* 创建parsers */
@@ -184,6 +267,7 @@ int main(int argc,char** argv){
     mpc_result_t r;
     if(mpc_parse("<stdin>", input, Lispy, &r)){
       lval* x= lval_read(r.output);
+      x=lval_eval(x);
       lval_println(x);
       mpc_ast_delete(r.output);
     }else{
@@ -198,42 +282,3 @@ int main(int argc,char** argv){
   return 0;
 }
 
-/* lval eval(mpc_ast_t* t){ */
-/*   /\* 数字直接返回值 *\/ */
-/*   if(strstr(t->tag, "number")){ */
-/*     errno = 0; */
-/*     long x = strtol(t->contents, NULL, 10); */
-/*     return errno !=ERANGE ? lval_num(x):lval_err(LERR_BAD_NUM); */
-/*   } */
-
-/*   /\* 表达式，计算其值 *\/ */
-/*   char* op=t->children[1]->contents; */
-
-/*   lval x =eval(t->children[2]); */
-
-/*   int i=3; */
-/*   while(strstr(t->children[i]->tag, "expr")){ */
-/*     x=eval_op(x, op, eval(t->children[i])); */
-/*     i++; */
-/*   } */
-/*   return x; */
-
-/* } */
-
-/* lval eval_op(lval x, char* op, lval y) { */
-
-/*   if(x.type ==LVAL_ERR) return x; */
-/*   if(y.type ==LVAL_ERR) return y; */
-
-/*   if (strcmp(op, "+") == 0 || strcmp(op, "add") == 0) { return lval_num( x.num + y.num); } */
-/*   if (strcmp(op, "-") == 0 || strcmp(op, "sub") == 0) { return lval_num( x.num - y.num); } */
-/*   if (strcmp(op, "*") == 0 || strcmp(op, "mul") == 0) { return lval_num( x.num * y.num); } */
-/*   if (strcmp(op, "/") == 0 || strcmp(op, "div") == 0) { */
-/*     if(y.num!=0){ */
-/*       return lval_num( x.num/y.num); */
-/*     }else{ */
-/*       return lval_err(LERR_DIV_ZERO); */
-/*     } */
-/*   } */
-/*   if (strcmp(op, "%") == 0) { return lval_num( x.num % y.num); } */
-/* } */
