c380b7d02107d801827db45f8fc9e4606fdfb627
Richard <a247820400@gmail.com>
2018-08-28 20:47:48 +0800

    chapter 6

---
+++ TSPL/6.1.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+;;操作符与对象
+
+;;这一章节介绍了一些对象上的操作,包括了lists, numbers, characters, strings, vectors, bytevectors, symbols, booleans, hashtables, 以及 enumerations
+;;第一章是常亮和引用
+;;第二章是相等的判断
+;;剩下的章节是介绍了
+
+;第一章放在博客上
+
